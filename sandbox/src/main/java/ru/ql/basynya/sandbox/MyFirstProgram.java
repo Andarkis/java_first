424808533fffd181b8d6470e3392a04d6df21553
Andarkis <Standartis@gmail.com>
2018-06-11 17:26:36 +0300

    Добавлены функции вычисления площади

--- sandbox/src/main/java/ru/ql/basynya/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/ql/basynya/sandbox/MyFirstProgram.java
@@ -3,7 +3,23 @@ package ru.ql.basynya.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+	  hello("Andrey");
+	  double l = 3;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+    double a = 2;
+    double b = 5;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
 	}
 
+	public static void hello(String somebody) {
+    System.out.println("Hello " + somebody + "!");
+  }
+
+  public static double area(double l){
+	  return l * l;
+  }
+
+  public static double area(double a, double b){
+    return a * b;
+  }
 }
